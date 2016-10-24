05587de34afa011fa200a739383c35fa3f860fa2
Dominik Liebler <liebler.dominik@gmail.com>
2016-10-24 20:21:53 +0200

    m0re docs

--- lib/kindle_clippings.ex
+++ lib/kindle_clippings.ex
@@ -1,7 +1,12 @@
 defmodule KindleClippings do
   @moduledoc """
-    a struct for clippings
+    A module to parse Amazon Kindle MyClipping.txt files and generate
+    beautiful HTML reports from it. It just shows how easy it is to parse
+    structured text files in Elixir.
   """
 
+  @doc """
+  a struct for parsed clippings
+  """
   defstruct [book: nil, author: nil, created: nil, body: nil]
 end
